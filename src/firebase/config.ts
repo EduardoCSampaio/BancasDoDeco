08c36154b162f3a9f01391738eb14efeb65c5175
Eduardo Campos <sampaioeduardocamposs@gmail.com>
2025-10-30 23:38:23 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "bancasdodeco",
-  "appId": "1:380482596956:web:d87654f593b137f8835261",
-  "storageBucket": "bancasdodeco.appspot.com",
-  "apiKey": "AIzaSyCV36UT3s5v3yv7-W_82K48f3uprx3g6sM",
-  "authDomain": "bancasdodeco.firebaseapp.com",
-  "messagingSenderId": "380482596956"
+  "projectId": "studio-2776215023-ccbba",
+  "appId": "1:777876758511:web:b2f2f8616fd27569f1601a",
+  "apiKey": "AIzaSyA8mr1CW-7yskrAOtg-qat4Jj_lDK1RKx0",
+  "authDomain": "studio-2776215023-ccbba.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "777876758511"
 };
