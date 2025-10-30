dd4d54d0d1bd369a768893fb76018748c45f0967
Eduardo Campos <sampaioeduardocamposs@gmail.com>
2025-10-30 04:55:15 +0000

    Try fixing this error: `Build Error: Export RouletteWheelIcon doesn't ex

--- src/app/dashboard/_components/nav.tsx
+++ src/app/dashboard/_components/nav.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
-import { Users, RouletteWheelIcon, Trophy } from 'lucide-react';
+import { Users, Trophy } from 'lucide-react';
+import { RouletteWheelIcon } from '@/components/icons';
 
 const links = [
   { name: 'Participantes', href: '/dashboard', icon: Users },
