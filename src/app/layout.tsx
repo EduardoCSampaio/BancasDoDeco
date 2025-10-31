2f79923da922c7fb179106799d54c57867b19897
Eduardo Campos <sampaioeduardocamposs@gmail.com>
2025-10-31 02:05:36 +0000

    Será que podemos mudar o favicon tmb?
    Queria colocar uma pomba kakaka

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const doveFavicon = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🕊️</text></svg>`;
+
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+       <head>
+        <link rel="icon" href={doveFavicon} />
+      </head>
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
