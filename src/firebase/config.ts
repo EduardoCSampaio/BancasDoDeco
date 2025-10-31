c64927cae75dc3bdb5b784429c6552d3b2b29c08
Eduardo Campos <sampaioeduardocamposs@gmail.com>
2025-10-31 00:17:59 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-2776215023-ccbba",
-  "appId": "1:777876758511:web:b2f2f8616fd27569f1601a",
-  "apiKey": "AIzaSyA8mr1CW-7yskrAOtg-qat4Jj_lDK1RKx0",
-  "authDomain": "studio-2776215023-ccbba.firebaseapp.com",
+  "projectId": "studio-2776215023-4252d",
+  "appId": "1:149043564543:web:d210ab507cc41e608f21ca",
+  "apiKey": "AIzaSyCytpy8Y8e-TX_Mom3WI9dKXwUu4MPznAg",
+  "authDomain": "studio-2776215023-4252d.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "777876758511"
+  "messagingSenderId": "149043564543"
 };
